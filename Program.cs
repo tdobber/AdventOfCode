90a454c4ef9fb0a1193c39be85cdf0aebd383a74
Timo <Timo@Timo>
2020-11-30 09:27:14 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which day would you like to run?");
+            int dayToRun = Convert.ToInt16(Console.ReadLine());
+
+            string cwd = Directory.GetCurrentDirectory();
+            string readFile;
+
+            if (cwd.EndsWith('9'))
+                readFile = "Inputs//";
+            else
+                readFile = "..//..//..//Inputs//";
+
+            switch (dayToRun)
+            {
+                //case 1:
+                //    Day1.Start(readFile);
+                //    break;
+
+                default:
+                    Console.WriteLine("That day is not supported yet");
+                    break;
+            }
+        }
+    }
+}
