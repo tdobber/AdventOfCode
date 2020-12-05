7f6946184c5f3265448fb2a087d17ecd892dc4db
tdobber <timo.dobber@student.uva.nl>
2020-12-05 19:43:14 +0100

    Solution day 5

--- Day5.cs
+++ Day5.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -8,11 +9,8 @@ namespace AdventOfCode2020
     {
         public class BoardingPass
         {
-            public string OriginalSeat { get; set; }
-            public byte[] OriginalRow { get; set; }
-            public byte[] OriginalColumn { get; set; }
-            public int Row { get; set; }
-            public int Column { get; set; }
+            public byte Row { get; set; }
+            public byte Column { get; set; }
             public int SeatId
             {
                 get
@@ -26,16 +24,34 @@ namespace AdventOfCode2020
         {
             List<BoardingPass> boardingPasses = new List<BoardingPass>();
 
+            foreach(string line in input)
+            {
+                string binaryLine = line.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1');
+
+                boardingPasses.Add(new BoardingPass
+                {
+                    Row = Convert.ToByte(binaryLine[0..7], 2),
+                    Column = Convert.ToByte(binaryLine[7..10], 2)
+                });
+            }
+
             return boardingPasses;
         }
  
         private static void Part1(List<string> input)
         {
+            List<BoardingPass> boardingPasses = ReadBoardingPasses(input);
+
+            Console.WriteLine($"The highest seat id is {boardingPasses.Max(pass => pass.SeatId)}.");
         }
 
         private static void Part2(List<string> input)
         {
-         
+            List<BoardingPass> boardingPasses = ReadBoardingPasses(input);
+
+            BoardingPass boardingPass = boardingPasses.Where(pass => !boardingPasses.Any(plusOne => plusOne.SeatId == pass.SeatId + 1) &&
+                                                                boardingPasses.Any(plusTwo => plusTwo.SeatId == pass.SeatId + 2)).FirstOrDefault();
+            Console.WriteLine($"The id of your seat is {boardingPass.SeatId + 1}.");
         }
 
 
