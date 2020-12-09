a639ba76448ab679c5d2a4d50d74ea0182afb77d
Timo <Timo@Timo>
2020-12-09 08:35:23 +0100

    Solution for day 9

--- 2020/Day9.cs
+++ 2020/Day9.cs
@@ -7,25 +7,56 @@ namespace AdventOfCode._2020
 {
     public static class Day9
     {
-        private static List<string> Input => InputHelper.GetInputListString(2020, 9).ToList();
+        private static List<long> Input => InputHelper.GetInputListLong(2020, 9).ToList();
 
-        
-
-        private static void Part1()
+        private static int Part1()
         {
+            List<long> preamble = Input.ToArray()[0..25].ToList();
 
+            for (int i = 25; i < Input.Count(); i++)
+            {
+                if (preamble.Any(num => preamble.Contains(Input[i] - num) && (Input[i] / 2 != num)))
+                {
+                    preamble.RemoveAt(0);
+                    preamble.Add(Input[i]);
+                }
+                else
+                {
+                    Console.WriteLine($"The following number is not correct {Input[i]}");
+                    return i;
+                }
+            }
+
+            return 0;
         }
 
-        private static void Part2()
+        private static void Part2(int index)
         {
+            long errorNumber = Input[index];
+            List<long> validRange = Input.ToArray()[0..index].ToList();
 
+            for (int i = 0; i < index - 1; i++)
+            {
+                for (int j = i + 1; j < index; j++)
+                {
+                    List<long> range = validRange.ToArray()[i..j].ToList();
+                    if (range.Sum() == errorNumber)
+                    {
+                        Console.WriteLine($"The smallest and largest numbers of the range are {range.Min()} and {range.Max()} which add up to {range.Min() + range.Max()}");
+                    }
+                    else if(range.Sum() > errorNumber)
+                    {
+                        break;
+                    }
+                }
+            }
         }
 
         public static void Start()
         {
-            Part1();
+            int index = Part1();
 
-            Part2();
+            Part2(index);
         }
     }
 }
--- Helpers/InputHelper.cs
+++ Helpers/InputHelper.cs
@@ -11,6 +11,9 @@ namespace AdventOfCode.Helpers
         
         public static List<int> GetInputListInt(int year, int day) =>
             File.ReadAllLines($"..\\..\\..\\{year}\\Inputs\\Day{day}.txt").Select(int.Parse).ToList();
+        
+        public static List<long> GetInputListLong(int year, int day) =>
+            File.ReadAllLines($"..\\..\\..\\{year}\\Inputs\\Day{day}.txt").Select(long.Parse).ToList();
 
         public static string GetInputString(int year, int day) =>
             File.ReadAllText($"..\\..\\..\\{year}\\Inputs\\Day{day}.txt");
