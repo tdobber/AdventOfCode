182b707166892bc36a8c9d93a8662536061b368c
tdobber <timo.dobber@student.uva.nl>
2020-12-12 14:17:54 +0100

    Solution day 11 part 1 and failed try part 2

--- 2020/Day11.cs
+++ 2020/Day11.cs
@@ -17,11 +17,15 @@ namespace AdventOfCode._2020
             public int SizeY { get; set; }
             public int Rounds { get; set; } = 0;
 
-            public void ProcessRound()
+            public void ProcessRound(int part = 1)
             {
-                while (Rounds != 5)
+                while (true)
                 {
-                    CopyArraysAndCheck();
+                    if (CopyArraysAndCheck())
+                    {
+                        break;
+                    }
+
                     for (int i = 0; i < SizeY; i++)
                     {
                         for (int j = 0; j < SizeX; j++)
@@ -36,10 +40,21 @@ namespace AdventOfCode._2020
                                     break;
 
                                 case '#':
-                                    if (CheckOccupiedSeats(i, j))
+                                    if (part == 1)
                                     {
-                                        Seats[i, j] = 'L';
+                                        if (CheckOccupiedSeats(i, j))
+                                        {
+                                            Seats[i, j] = 'L';
+                                        }
                                     }
+                                    else
+                                    {
+                                        if (CheckOccupiedSeatsStrict(i, j))
+                                        {
+                                            Seats[i, j] = 'L';
+                                        }
+                                    }
+                                    
                                     break;
 
                                 case '.':
@@ -49,11 +64,29 @@ namespace AdventOfCode._2020
                                     Console.WriteLine("Should not happen, debug...");
                                     break;
                             }
+
+                            Console.Write(OldSeats[i, j]);
                         }
+                        Console.WriteLine();
                     }
 
-                    Rounds++;
+                    Console.WriteLine();
+
                 }
+
+                int occupiedSeats = 0;
+                for (int i = 0; i < SizeY; i++)
+                {
+                    for (int j = 0; j < SizeX; j++)
+                    {
+                        if (Seats[i, j] == '#')
+                        {
+                            occupiedSeats++;
+                        }
+                    }
+                }
+
+                Console.WriteLine($"The amount of occupied seats after stabilization is {occupiedSeats}.");
 
                 return;
             }
@@ -111,26 +144,199 @@ namespace AdventOfCode._2020
 
             private bool CheckOccupiedSeats(int originalI, int originalJ)
             {
-                for (int counter = -4; counter <= 4; counter++)
+                int takenSeats = 0;
+                for (int i = originalI - 1; i <= originalI + 1; i++)
                 {
-                    if (originalI + counter < 0 || originalJ + counter < 0 || originalI + counter >= SizeY || originalJ + counter >= SizeX || originalI - counter < 0 || originalI - counter >= SizeY)
+                    if (i < 0 || i >= SizeY)
                     {
                         continue;
                     }
-                    // y-axis
-                    else if (OldSeats[originalI + counter, originalJ] != '#' ||
-                        // x-axis
-                        OldSeats[originalI, originalJ + counter] != '#' ||
-                        // Top left to bottom right
-                        OldSeats[originalI + counter, originalJ + counter] != '#' ||
-                        // Bottom left to top right
-                        OldSeats[originalI - counter, originalJ + counter] != '#')
+                    else
                     {
+                        for (int j = originalJ - 1; j <= originalJ + 1; j++)
+                        {
+                            if (j < 0 || j >= SizeX || (i == originalI && j == originalJ))
+                            {
+                                continue;
+                            }
+                            else if (OldSeats[i, j] == '#')
+                            {
+                                takenSeats++;
+                            }
+                        }
+                    }
+                }
+
+                if (takenSeats >= 4)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            private bool CheckOccupiedSeatsStrict(int originalI, int originalJ)
+            {
+                int takenSeats = 0;
+
+                bool seat1Found = false;
+                bool seat2Found = false;
+                bool seat3Found = false;
+                bool seat4Found = false;
+                bool seat5Found = false;
+                bool seat6Found = false;
+                bool seat7Found = false;
+                bool seat8Found = false;
+
+                for (int i = 1; i < SizeX && i < SizeY; i++)
+                {
+                    if (!seat1Found)
+                    {
+                        if (originalI - i < 0)
+                        {
+                            seat1Found = true;
+                        }
+                        else if(OldSeats[originalI - i, originalJ] != '.')
+                        {
+                            if (OldSeats[originalI - i, originalJ] == '#')
+                            {
+                                takenSeats++;
+                            }
+                            seat1Found = true;
+                        }
+                    }
+
+                    if (!seat2Found)
+                    {
+                        if (originalI - i < 0 || originalJ + i >= SizeX)
+                        {
+                            seat2Found = true;
+                        }
+                        else if (OldSeats[originalI - i, originalJ + i] != '.')
+                        {
+                            if (OldSeats[originalI - i, originalJ + i] == '#')
+                            {
+                                takenSeats++;
+                            }
+                            seat2Found = true;
+                        }
+                    }
+
+                    if (!seat3Found)
+                    {
+                        if (originalJ + i >= SizeX)
+                        {
+                            seat3Found = true;
+                        }
+                        else if (OldSeats[originalI, originalJ + i] != '.')
+                        {
+                            if (OldSeats[originalI, originalJ + i] == '#')
+                            {
+                                takenSeats++;
+                            }
+                            seat3Found = true;
+                        }
+                    }
+
+                    if (!seat4Found)
+                    {
+                        if (originalI + i >= SizeY || originalJ + i >= SizeX)
+                        {
+                            seat4Found = true;
+                        }
+                        else if (OldSeats[originalI + i, originalJ + i] != '.')
+                        {
+                            if (OldSeats[originalI + i, originalJ + i] == '#')
+                            {
+                                takenSeats++;
+                            }
+                            seat4Found = true;
+                        }
+                    }
+
+                    if (!seat5Found)
+                    {
+                        if (originalI - i < 0)
+                        {
+                            seat5Found = true;
+                        }
+                        else if (OldSeats[originalI - i, originalJ] != '.')
+                        {
+                            if (OldSeats[originalI - i, originalJ] == '#')
+                            {
+                                takenSeats++;
+                            }
+                            seat5Found = true;
+                        }
+                    }
+
+                    if (!seat6Found)
+                    {
+                        if (originalI + i >= SizeY || originalJ - i < 0)
+                        {
+                            seat6Found = true;
+                        }
+                        else if (OldSeats[originalI + i, originalJ - i] != '.')
+                        {
+                            if (OldSeats[originalI + i, originalJ - i] == '#')
+                            {
+                                takenSeats++;
+                            }
+                            seat6Found = true;
+                        }
+                    }
+
+                    if (!seat7Found)
+                    {
+                        if (originalJ - i < 0)
+                        {
+                            seat7Found = true;
+                        }
+                        else if (OldSeats[originalI, originalJ - i] != '.')
+                        {
+                            if (OldSeats[originalI, originalJ - i] == '#')
+                            {
+                                takenSeats++;
+                            }
+                            seat7Found = true;
+                        }
+                    }
+
+                    if (!seat8Found)
+                    {
+                        if (originalI - i < 0 || originalJ - i < 0)
+                        {
+                            seat8Found = true;
+                        }
+                        else if (OldSeats[originalI - i, originalJ - i] != '.')
+                        {
+                            if (OldSeats[originalI - i, originalJ - i] == '#')
+                            {
+                                takenSeats++;
+                            }
+                            seat8Found = true;
+                        }
+                    }
+
+                    if (seat1Found &&
+                        seat2Found &&
+                        seat3Found &&
+                        seat4Found &&
+                        seat5Found &&
+                        seat6Found &&
+                        seat7Found &&
+                        seat8Found)
+                    {
+                        if (takenSeats >= 5)
+                        {
+                            return true;
+                        }
+
                         return false;
                     }
                 }
 
-                return true;
+                return false;
             }
         }
 
@@ -162,7 +368,10 @@ namespace AdventOfCode._2020
 
         private static void Part2()
         {
+            SeatMap map = LoadSeatMap(Input.Count, Input[0].Length);
 
+            map.ProcessRound(2);
+            return;
         }
 
         public static void Start()
--- Helpers/InputHelper.cs
+++ Helpers/InputHelper.cs
@@ -6,8 +6,16 @@ namespace AdventOfCode.Helpers
 {
     public static class InputHelper
     {
-        public static List<string> GetInputListString(int year, int day) =>
-            File.ReadAllLines($"..\\..\\..\\{year}\\Inputs\\Day{day}.txt").ToList();
+        public static List<string> GetInputListString(int year, int day)
+        {
+            if (File.Exists($"..\\..\\..\\{year}\\Inputs\\Day{day}.txt")) {
+                return File.ReadAllLines($"..\\..\\..\\{year}\\Inputs\\Day{day}.txt").ToList();
+            }
+            else
+            {
+                return File.ReadAllLines($"../../../{year}/Inputs/Day{day}.txt").ToList();
+            }
+        }
         
         public static List<int> GetInputListInt(int year, int day) =>
             File.ReadAllLines($"..\\..\\..\\{year}\\Inputs\\Day{day}.txt").Select(int.Parse).ToList();
